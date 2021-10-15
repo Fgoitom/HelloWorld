d633b6dc681997f7fcc1e97e0c3492c3fdb1a7fa
fgoitom <fevengoitom9@gmail.com>
2021-10-15 12:54:23 -0500

    October 15, 2021

---
+++ src/cs201labs/lab11/Ques1.java
@@ -0,0 +1,36 @@
+//package cs201labs.lab11;
+
+//import java.util.Scanner;
+//import java.util.InputMismatchException;
+//
+//
+//public class Ques1 {
+//    public static void main(String[] args) {
+//
+
+//public class Exception {
+//    Scanner scanner = new Scanner(System.in);
+//    try {
+//        int num = scanner.nextInt();
+//        if ( num!=0){
+//            throw new java.lang.Exception("Not zero");
+//        }
+//        System.out.println("I'm happy with the input.");
+//
+//    } catch (
+//    InputMismatchException e){
+//        System.out.println("Invalid Entry");
+//    }catch (Exception e){
+//        System.out.println(" Error: " + e.getMessage());
+//    }
+//
+//}
+//            a). -1;
+//    Output : Error:Not zero
+//            b). 0;
+//    Output : I'm happy with the input.
+//            c). 12xy;
+//    Output : Invalid entry
+//
+//    }
+//}
